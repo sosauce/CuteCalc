e906a573c4ca9e6ce6d4864d9e790fd0ff168f18
notKamui <teillard.jimmy@outlook.fr>
2024-03-01 23:22:57 +0100

    why ???

--- app/src/main/java/com/sosauce/cutecalc/screens/Calculator.kt
+++ app/src/main/java/com/sosauce/cutecalc/screens/Calculator.kt
@@ -1,11 +1,4 @@
-@file:OptIn(
-    ExperimentalMaterial3Api::class, ExperimentalMaterial3Api::class,
-    ExperimentalMaterial3Api::class, ExperimentalMaterial3Api::class,
-    ExperimentalMaterial3Api::class, ExperimentalMaterial3Api::class,
-    ExperimentalMaterial3Api::class, ExperimentalMaterial3Api::class,
-    ExperimentalMaterial3Api::class, ExperimentalMaterial3Api::class,
-    ExperimentalMaterial3Api::class, ExperimentalMaterial3Api::class
-)
+@file:OptIn(ExperimentalMaterial3Api::class)
 
 package com.sosauce.cutecalc.screens
 
